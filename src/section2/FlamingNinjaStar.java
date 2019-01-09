f95661aecb45675be66d2ab28831eadaff4857d0
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-01-09 13:54:55 -0800

    pentagon and ninja star

--- src/section2/FlamingNinjaStar.java
+++ src/section2/FlamingNinjaStar.java
@@ -10,37 +10,35 @@ public class FlamingNinjaStar {
 		int flameSize = 200;		//the length of the flaming arms
 		
 		// 1. Make a new robot, and set it's pen down.
-
+Robot bmo= new Robot();
+bmo.hide();
 		// *14. Use the methods setX and setY to move the ninja star into the center of the screen
-		
+		bmo.setX(650);
+		bmo.setY(300);
+		bmo.penDown();
 		// *15. Make some adjustments to see what other kinds of shapes you can make.
 
 		// 12. Set the robot speed to 100
-
+bmo.setSpeed(100);
 		// 13. Use a for loop to repeat all of the code below 25 times
-
+for(int i=0;i<25;i++) {
 			// 2. Turn the robot 1/8 of a circle
-		
+		bmo.turn(360/8);
 			// 3. Move the robot 64 pixels
-
+bmo.move(64);
 			// 4. Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			
+			bmo.turn(-40);
 			// 5. Move the robot the distance in the variable flameSize
-			
+			bmo.move(200);
 			// 6. Turn the robot 170 degrees
-			
+			bmo.turn(170);
 			// 7. Move the robot the distance in the variable flameSize (again)
-			
+			bmo.move(200);
 			// 8. Turn the robot 64 degrees to the right
-			
+			bmo.turn(64);
 			// 9. Move the robot the distance in the variable baseSize
-			
-			// 10. Check that your shape is the same as Figure 1. This is one arm of the ninja star.
-			
+			bmo.move(baseSize);
 			// 11. Color your ninja star like Figure 2.
-		
-	}
-
-}
-
-
+		bmo.setPenColor(0, 0, 0);
+}}}
+	
--- src/section2/PentagonCrazy.java
+++ src/section2/PentagonCrazy.java
@@ -18,20 +18,20 @@ hal.setSpeed(100);
 		// 9. Set the pen to a color that you like for the shape
 hal.setRandomPenColor();
 		// 4. Make a variable for the number of sides you want (can’t test this one)
-
+int sides=5;
 		// 5. Make a second variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6
-
+int angle=360/sides;
 		// 7. Use a for loop to repeat steps #2 to #11, 200 times. When this is done you should see a pentagon
-
+for(int i=0;1<5;i++) {
 			// 2. Move the robot 200 pixels
 	
 			// 10. Change the previous line of code to make the robot move "i" pixels instead of 200 
-	
+	hal.move(i);
 			// 6. Turn the robot the amount in your angle variable
-	
+	hal.turn(angle);
 			// 11. Turn the robot one more degree
-
-	}
+	hal.turn(1);
+	}}
 
 	// Variations:
 	// *12. make the pattern really huge
